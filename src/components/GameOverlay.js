513369d0b9abf951ca6cf93929b70fc9038dbea1
JamesKevinJones <117106574+JamesKevinJones@users.noreply.github.com>
2024-06-11 22:18:05 +0530

    Add files via upload

---
+++ src/components/GameOverlay.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import TryAgainLogo from '../assets/img/try-again.gif'
+
+ const GameOverlay = ({onRestart,board}) => {
+    if (board.hasWon()){
+            return <div className="tile2048"></div>
+    }else if(board.hasLost()){
+        return( 
+            <div className="gameOver">
+                <img 
+                    src={TryAgainLogo} 
+                    alt="Try Again" 
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                        cursur: 'pointer',
+                    }}
+                />
+            </div>
+        );
+    }
+    return null;
+  
+};
+
+export default GameOverlay;
